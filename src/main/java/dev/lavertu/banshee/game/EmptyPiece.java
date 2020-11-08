c8c4a5f7349ae96625853ce88a3a1f7005e3e37e
fraasty <njczarnecki@gmail.com>
2020-11-08 16:51:28 -0700

    Renamed Cart.java to Chariot.java

---
+++ src/main/java/dev/lavertu/banshee/game/EmptyPiece.java
@@ -0,0 +1,10 @@
+import dev.lavertu.banshee.game.Color;
+import dev.lavertu.banshee.game.Coordinate;
+
+public class EmptyPiece {
+	private boolean isCaptured = false;
+	private boolean isFaceUp = false;
+	private int rank = 1;
+	private Color color;
+	private Coordinate position;
+}
---
+++ src/main/java/dev/lavertu/banshee/game/Emptypiece.java
@@ -0,0 +1,5 @@
+public class Emptypiece { 
+	public static void main(String[] args) {
+
+	}
+}
