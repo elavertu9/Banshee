8ffe5911c394f2e7c5ca7680ff5375be336198f4
elavertu9 <elavertu@rams.colostate.edu>
2020-11-24 19:55:07 -0700

    Added spring config and heartbeat endpoint

--- src/main/java/dev/lavertu/banshee/game/Main.java
+++ src/main/java/dev/lavertu/banshee/game/Main.java
@@ -1,21 +1,32 @@
 package dev.lavertu.banshee.game;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
 public class Main
 {
-    public static void commandParserTesting(Game game) {
+    public static void commandParserTesting() {
+        Player player1 = new Player("Player 1");
+        Player player2 = new Player("Player 2");
+        Game game = new Game(player1, player2);
+        System.out.println(game.printGameBoard());
         ServerCommandParser commandParser = new ServerCommandParser(game);
         commandParser.listen();
     }
 
     // This class is for testing purposes
     public static void main(String[] args) {
-        Player player1 = new Player("Evan");
-        Player player2 = new Player("Paul");
+        //commandParserTesting();
+        SpringApplication.run(Main.class, args);
+    }
 
-        Game game = new Game(player1, player2);
-
-        System.out.println(game.printGameBoard());
-
-        commandParserTesting(game);
+    @GetMapping("/api/hello")
+    public String sayHello(@RequestParam(value = "myName", defaultValue = "") String name) {
+        return String.format("Hello %s! The Banshee Server is up and running", name);
     }
 }
