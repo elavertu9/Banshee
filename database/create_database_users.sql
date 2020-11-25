e968dab07f67720ae65227ff493779c2c8e83f19
hmanja <haritsa.manja@gmail.com>
2020-11-25 15:43:05 -0700

    ocd

--- database/create_database_users.sql
+++ database/create_database_users.sql
@@ -2,7 +2,7 @@ CREATE ROLE banshee_owner WITH LOGIN PASSWORD $1;
 -- GRANT rds_superuser to banshee_owner;
 ALTER USER banshee_owner WITH SUPERUSER;
 
-CREATE ROLE read_only with LOGIN PASSWORD $2;
+CREATE ROLE read_only WITH LOGIN PASSWORD $2;
 GRANT CONNECT ON DATABASE "Banshee" TO read_only;
 GRANT USAGE ON SCHEMA public TO read_only;
 GRANT SELECT ON ALL TABLES IN SCHEMA public TO read_only;
