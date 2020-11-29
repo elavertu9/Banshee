15ac1d7402c27f72a871a88515873dd6dd0a1ca7
hmanja <haritsa.manja@gmail.com>
2020-11-28 18:54:34 -0700

    Update create_all_db_resources.sql

--- database/create_all_db_resources.sql
+++ database/create_all_db_resources.sql
@@ -41,6 +41,9 @@ CREATE TABLE public.users
     first_name text NOT NULl,
     last_name text NOT NULL,
     email_address text NOT NULL,
+    wins bigint NOT NULL,
+    losses bigint NOT NULL,
+    games_played bigint NOT NULL,
     create_date timestamp with time zone NOT NULL,
     update_date timestamp with time zone NOT NULL
 );
@@ -53,6 +56,12 @@ CREATE TABLE public.games
     game_id uuid NOT NULL PRIMARY KEY,
     game_object character varying NOT NULL,
     finished boolean NOT NULL,
+    user1_color text NOT NULL,
+    user2_color text NOT NULL,
+    winner int,
+    is_forfeit boolean NOT NULL,
+    forfeit_user smallint,
+    turn smallint NOT NULL,
     create_date timestamp with time zone NOT NULL,
     update_date timestamp with time zone NOT NULL,
     user1_id uuid NOT NULL REFERENCES users (user_id) ON UPDATE NO ACTION ON DELETE CASCADE,
@@ -62,7 +71,7 @@ ALTER TABLE public.games OWNER to postgres;
 COMMENT ON TABLE public.games IS 'Keeps track of Banshee games';
 
 -- Insert default Users records
- INSERT INTO public.users(user_id, username, first_name, last_name, email_address, create_date, update_date)
- 	VALUES (uuid_generate_v4(), 'User1', 'User', 'One', 'User1@gmail.com', current_timestamp, current_timestamp);
- INSERT INTO public.users(user_id, username, first_name, last_name, email_address, create_date, update_date)
-    VALUES (uuid_generate_v4(), 'User2', 'User', 'Two', 'User2@gmail.com', current_timestamp, current_timestamp);
+ INSERT INTO public.users(user_id, username, first_name, last_name, email_address, wins, losses, games_played, create_date, update_date)
+ 	VALUES (uuid_generate_v4(), 'User1', 'User', 'One', 'User1@gmail.com', 0, 0, 0, current_timestamp, current_timestamp);
+ INSERT INTO public.users(user_id, username, first_name, last_name, email_address, wins, losses, games_played, create_date, update_date)
+    VALUES (uuid_generate_v4(), 'User2', 'User', 'Two', 'User2@gmail.com', 0, 0, 0, current_timestamp, current_timestamp);
